9c9e6c413ac03a441f3bd8d49622d371a30bb3a7
juliavitoriasantos <cruz.vitoria@aluno.ifsp.edu.br>
2024-09-18 12:46:14 -0300

    indo

---
+++ Salão_Beleza/Model/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salão_Beleza.Model
+{
+   internal class Cliente { 
+    public int CodProperty {get; set; }
+    public string FullName {get; set;}
+    public int Telephone {get; set;}
+    public string Email {get; set;}
+    public string Username {get; set;}
+    public string Password {get; set;}
+
+        public Cliente(int codproperty, string fullname, int telephone, string email, string username, string password)
+        {
+            CodProperty = codproperty;
+            FullName = fullname;
+            Telephone = telephone;
+            Email = email;
+            Username = username;
+            Password = password;
+        }
+    }
+}
